2280072994c217f20c4f17c1cc8201d4ef04bac4
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2025-03-02 17:41:59 -0500

    remove broken history

--- src/features/Panel/Panel.tsx
+++ src/features/Panel/Panel.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { PanelButton } from '@/components/Button/PanelButton';
 import { Separator } from '@/components/Separator/Separator';
@@ -13,46 +13,15 @@ interface PanelProps {
 
 export const PANEL_HEIGHT = 100;
 
-export const Panel: FC<PanelProps> = ({ stageRef }) => {
-  const [history, setHistory] = useState<any[]>([]);
-  const [historyIndex, setHistoryIndex] = useState(-1);
-
-  const saveHistory = () => {
-    const json = stageRef.current.toJSON();
-    const newHistory = history.slice(0, historyIndex + 1);
-    newHistory.push(json);
-    setHistory(newHistory);
-    setHistoryIndex(newHistory.length - 1);
-  };
-
-  const undo = () => {
-    if (historyIndex > 0) {
-      setHistoryIndex(historyIndex - 1);
-      stageRef.current?.destroyChildren();
-      stageRef.current?.draw();
-      stageRef.current?.load(history[historyIndex - 1]);
-    }
-  };
-
-  const redo = () => {
-    if (historyIndex < history.length - 1) {
-      setHistoryIndex(historyIndex + 1);
-      stageRef.current?.destroyChildren();
-      stageRef.current?.draw();
-      stageRef.current?.load(history[historyIndex + 1]);
-    }
-  };
+export const Panel: FC<PanelProps> = () => {
   return (
     <div className="flex justify-between items-center panel-wrapper px-3 border-b border-zinc-300">
       <div className="font-semibold">Industrial Pathway</div>
       <div className="flex">
-        <PanelButton onClick={undo} disabled={historyIndex <= 0}>
+        <PanelButton>
           <AiOutlineArrowLeft size={24} />
         </PanelButton>
-        <PanelButton
-          onClick={redo}
-          disabled={historyIndex >= history.length - 1}
-        >
+        <PanelButton>
           <AiOutlineArrowRight size={24} />
         </PanelButton>
         <Separator />
